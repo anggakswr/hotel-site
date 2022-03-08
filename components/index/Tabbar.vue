b8e441adddd39c1f1651b7caf922c6a5a6caf49c
anggakswr <222180023@student.unaki.ac.id>
2022-03-08 20:03:54 +0700

    Fix some minor issues

--- components/index/Tabbar.vue
+++ components/index/Tabbar.vue
@@ -97,7 +97,7 @@ export default {
       if (val === 'price') {
         this.priceBtnOn = !this.priceBtnOn
       } else {
-        this.$router.push({ ...this.$route.query, query: { sortby: val } })
+        this.$router.push({ query: { ...this.$route.query, sortby: val } })
       }
     },
   },
